f5849054021034dc809ea0c62b8c87395fde345c
No-Face-the-3rd <foodflare@yahoo.com>
2016-10-20 17:57:59 -0700

    start

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Vector2 min, max;
+    public KeyCode left, right, up, down;
+
+    public float shots;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 rot = transform.eulerAngles;
+        Vector3 pos = new Vector3();
+        if(Input.GetKey(left) && transform.position.x > min.x)
+        {
+
+        }
+    }
+}
